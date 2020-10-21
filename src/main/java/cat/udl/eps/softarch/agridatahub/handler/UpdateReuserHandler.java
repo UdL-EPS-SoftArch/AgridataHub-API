34377192a6b8739a2889332b0b654f6d76543ca9
robertmunne <robertmunne15@gmail.com>
2020-10-21 22:14:22 +0200

    CreateReuser updated

--- src/main/java/cat/udl/eps/softarch/agridatahub/handler/UpdateReuserHandler.java
+++ src/main/java/cat/udl/eps/softarch/agridatahub/handler/UpdateReuserHandler.java
@@ -1,4 +1,30 @@
 package cat.udl.eps.softarch.agridatahub.handler;
 
+import cat.udl.eps.softarch.agridatahub.domain.Reuser;
+import cat.udl.eps.softarch.agridatahub.exception.ForbiddenException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.rest.core.annotation.HandleBeforeSave;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RepositoryEventHandler
 public class UpdateReuserHandler {
+    private final Logger logger = LoggerFactory.getLogger(Reuser.class);
+
+    @HandleBeforeSave
+    public void handleReuserPreUpdate(Reuser reuser) {
+        logger.info("Before delete: {}", reuser.toString());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        logger.info("Username: {}", authentication.getAuthorities());
+
+        Reuser curr_reuser = ((Reuser)authentication.getPrincipal());
+
+        if (!curr_reuser.getId().equals(reuser.getId())){
+            throw new ForbiddenException();
+        }
+    }
 }
--- src/test/java/cat/udl/eps/softarch/agridatahub/steps/UpdateReuserStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/agridatahub/steps/UpdateReuserStepDefs.java
@@ -1,4 +1,49 @@
 package cat.udl.eps.softarch.agridatahub.steps;
 
+import cat.udl.eps.softarch.agridatahub.repository.ReuserRepository;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 public class UpdateReuserStepDefs {
+    final StepDefs stepDefs;
+    final ReuserRepository reuserRepository;
+
+    UpdateReuserStepDefs(StepDefs stepDefs, ReuserRepository reuserRepository) {
+        this.stepDefs = stepDefs;
+        this.reuserRepository = reuserRepository;
+    }
+
+    @When("I change the email of reuser {string} to {string}")
+    public void iChangeTheEmailOfReuserTo(String username, String email) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                patch("/reusers/{username}", username)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content((new JSONObject().put("email", email)).toString())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .with(AuthenticationStepDefs.authenticate())
+        ).andDo(print());
+    }
+
+    @And("It has been updated the email of reuser {string} to {string}")
+    public void itHasBeenUpdatedTheEmailOfReuserTo(String username, String email) throws Throwable {
+        stepDefs.result = stepDefs.mockMvc.perform(
+                get("/reusers/{username}", username)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(jsonPath("$.email", is(email)));
+    }
+
+    @And("Email of reuser {string} is still {string}")
+    public void emailOfReuserIsStill(String username, String email) throws Throwable {
+        itHasBeenUpdatedTheEmailOfReuserTo(username, email);
+    }
 }
