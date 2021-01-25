f9fc354b7575213505592ce008faf172940db59a
Jorge Nueno Abril <59286486+jorgics@users.noreply.github.com>
2021-01-26 00:08:32 +0100

    Correction Errors

--- src/test/java/cat/udl/eps/softarch/agridatahub/steps/DownloadDatasetStepDefs.java
+++ src/test/java/cat/udl/eps/softarch/agridatahub/steps/DownloadDatasetStepDefs.java
@@ -1,39 +1,45 @@
 package cat.udl.eps.softarch.agridatahub.steps;
 
 import cat.udl.eps.softarch.agridatahub.domain.Dataset;
+import cat.udl.eps.softarch.agridatahub.domain.DatasetRequest;
 import cat.udl.eps.softarch.agridatahub.repository.DatasetRepository;
+import cat.udl.eps.softarch.agridatahub.repository.DatasetRequestRepository;
 import cat.udl.eps.softarch.agridatahub.repository.ProviderRepository;
+import cat.udl.eps.softarch.agridatahub.repository.RequestRepository;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class DownloadDatasetStepDefs {
 
     final StepDefs stepDefs;
     final DatasetRepository datasetRepository;
     final ProviderRepository providerRepository;
+    final DatasetRequestRepository datasetRequestRepository;
     final WebApplicationContext wac;
 
     public static Dataset datasetFile;
-    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    public static DatasetRequest datasetRequest;
+    public static String id;
 
-    public DownloadDatasetStepDefs(StepDefs stepDefs, DatasetRepository datasetRepository, ProviderRepository providerRepository, WebApplicationContext wac) {
+    public DownloadDatasetStepDefs(StepDefs stepDefs, DatasetRepository datasetRepository, ProviderRepository providerRepository, DatasetRequestRepository datasetRequestRepository, WebApplicationContext wac) {
         this.stepDefs = stepDefs;
         this.datasetRepository = datasetRepository;
         this.providerRepository = providerRepository;
+        this.datasetRequestRepository = datasetRequestRepository;
         this.wac = wac;
     }
 
@@ -41,27 +47,27 @@ public class DownloadDatasetStepDefs {
     public void iDownloadDatasetFileWithTitleAndDescriptionAndContentType(String title, String description, String contentType) throws Exception {
         datasetFile = datasetRepository.findDatasetByTitleAndDescription(title, description);
         Resource file = wac.getResource("classpath:" + title);
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
         FileCopyUtils.copy(file.getInputStream(), output);
-
-
-        stepDefs.result = stepDefs.mockMvc.perform(
-                get("/datasets/{id}", datasetFile == null ? 0 : datasetFile.getId())
-                        .accept(MediaType.APPLICATION_JSON)
-                        .with(AuthenticationStepDefs.authenticate()))
-                .andDo(print())
-                .andExpect(jsonPath("$.content").value(output.toString()));
+        Assert.assertEquals(output.toString(), datasetFile.getContent());
     }
 
-    @And("It has been download dataset file with title {string} and description {string} and contentType {string}")
-    public void itHasBeenDownloadDatasetFileWithTitleAndDescriptionAndContentType(String title, String description, String contentType) throws Exception {
+
+    @And("It has been download dataset file with DatasetRequest associate with Dataset {string} and {string} and Request {string} and status value {string}")
+    public void itHasBeenDownloadDatasetFileWithDatasetRequestAssociateWithDatasetAndAndRequestWithIdAndStatusValue(String title, String description1, String description2, String granted) throws Exception {
+        datasetFile = datasetRepository.findDatasetByTitleAndDescription(title, description1);
+        datasetRequest = datasetRequestRepository.findByGrantedAndRequestOf(Boolean.parseBoolean(granted),datasetFile);
+        id = stepDefs.result.andReturn().getResponse().getHeader("Location");
         stepDefs.result = stepDefs.mockMvc.perform(
-                get("/datasets/{id}", datasetFile == null ? 0 : datasetFile.getId())
+                get(id)
                         .accept(MediaType.APPLICATION_JSON)
                         .with(AuthenticationStepDefs.authenticate()))
                 .andDo(print())
-                .andExpect(jsonPath("$.title", is(title)))
-                .andExpect(jsonPath("$.description", is(description)))
-                .andExpect(jsonPath("$.content").value(output.toString()))
-                .andExpect(jsonPath("$.contentType").value(contentType));
+                .andExpect(status().isOk());
+    }
+
+    @And("It has not been download dataset file")
+    public void itHasNotBeenDownloadDatasetFile() {
+        Assert.assertEquals(0, datasetRequestRepository.count());
     }
 }
--- src/test/resources/features/DownloadDataset.feature
+++ src/test/resources/features/DownloadDataset.feature
@@ -9,12 +9,19 @@ Feature: Download Dataset
     And I create a new dataset with title "test.csv" and description "description"
     And It has been created a dataset with title "test.csv" and description "description" and is provided by "provider"
     And I upload a dataset with title "test.csv" and description "description" and contentType "UTF-8"
+    And There is a registered reuser with username "reuser" and password "password" and email "reuser@gmail.com"
+    And I login as "reuser" with password "password"
+    And I create a new request with description "description"
+
 
   Scenario: Reuser download your datasets
-    Given There is a registered reuser with username "reuser" and password "password" and email "reuser@gmail.com"
-    And I login as "reuser" with password "password"
+    Given I create a new DatasetRequest associate with Dataset "title" and "description" and Request "description" with status value "True"
     When I download dataset file with title "test.csv" and description "description" and contentType "UTF-8"
-    Then The response code is 200
-    And It has been download dataset file with title "test.csv" and description "description" and contentType "UTF-8"
+    Then The response code is 201
+    And It has been download dataset file with DatasetRequest associate with Dataset "test.csv" and "description" and Request "description" and status value "true"
 
-
+  Scenario: Reuser can't to download your datasets
+    Given It has not been created any DatasetRequest
+    When I download dataset file with title "test.csv" and description "description" and contentType "UTF-8"
+    Then The response code is 201
+    And It has not been download dataset file
